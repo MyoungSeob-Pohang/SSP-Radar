0f6cc95a0ce99f415d9ef4232b2dbe1899a8be56
Myoungseob <myoungseob91@gmail.com>
2022-04-06 18:03:36 +0900

    block create

--- src/components/atoms/ImmuneBlock.js
+++ src/components/atoms/ImmuneBlock.js
@@ -1,5 +1,40 @@
 const ImmuneBlock = () => {
-    return <div className="ImmuneBlock">ImmuneBlock</div>;
+    return (
+        <div className="ImmuneBlock">
+            <div className="ImmuneBlock_inner">
+                <div className="ImmuneBlockItems">
+                    {/* 1 */}
+                    <div className="ImmuneBlockItem">
+                        <div className="ImmuneBlockItem_inner">
+                            <h3>IMMUNE TO LIGHT</h3>
+                            <div className="ImmuneBlockItemBg"></div>
+                        </div>
+                    </div>
+                    {/* 2 */}
+                    <div className="ImmuneBlockItem">
+                        <div className="ImmuneBlockItem_inner">
+                            <h3>IMMUNE TO DEBRIS</h3>
+                            <div className="ImmuneBlockItemBg"></div>
+                        </div>
+                    </div>
+                    {/* 3 */}
+                    <div className="ImmuneBlockItem">
+                        <div className="ImmuneBlockItem_inner">
+                            <h3>IMMUNE TO SMOKE</h3>
+                            <div className="ImmuneBlockItemBg"></div>
+                        </div>
+                    </div>
+                    {/* 4 */}
+                    <div className="ImmuneBlockItem">
+                        <div className="ImmuneBlockItem_inner">
+                            <h3>IMMUNE TO FLUIDS</h3>
+                            <div className="ImmuneBlockItemBg"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default ImmuneBlock;
